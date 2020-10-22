b159723cd23544ab6d0ba8c0868468ee0bd71979
krishnakanthati <krishnakanthati@outlook.com>
2020-10-22 11:31:26 +0530

    jokes generator

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, {useState, useEffect} from 'react';
+
+const API_URL = `http://api.icndb.com/jokes/random`
+
+function App() {
+  const [joke, setJoke] = useState('');
+
+  const generateJoke = () => {
+    fetch(API_URL)
+    .then(res => res.json())
+    .then(data => setJoke(data.value.joke))
+  }
+
+  useEffect(() => {
+    generateJoke()
+  }, [])
+
+  return (
+    <div className="box">
+      <h3>Chuck Norris Jokes Generator</h3>
+      <p dangerouslySetInnerHTML={{__html: joke}} />
+      <button onClick = {generateJoke}>
+        {`Get Me A Joke 😄`}
+      </button>
+    </div>
+  );
+}
+
+export default App;
