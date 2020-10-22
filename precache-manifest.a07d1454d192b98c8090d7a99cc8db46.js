47fc239e5559fc2a984c291ef2e2780d3bd0be67
krishnakanthati <krishnakanthati@outlook.com>
2020-10-22 11:40:16 +0530

    Updates

---
+++ precache-manifest.a07d1454d192b98c8090d7a99cc8db46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1a2270696ab71d294791c07b7ca62ef",
+    "url": "/Jokes-Generator/index.html"
+  },
+  {
+    "revision": "e0770b32cac436a62a59",
+    "url": "/Jokes-Generator/static/css/main.3ed0ca8d.chunk.css"
+  },
+  {
+    "revision": "23c6acefb80b36406e5c",
+    "url": "/Jokes-Generator/static/js/2.cef14cd2.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Jokes-Generator/static/js/2.cef14cd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0770b32cac436a62a59",
+    "url": "/Jokes-Generator/static/js/main.ee50dfd9.chunk.js"
+  },
+  {
+    "revision": "af9ab8700edbcac2ec68",
+    "url": "/Jokes-Generator/static/js/runtime-main.af1b64e1.js"
+  }
+]);
